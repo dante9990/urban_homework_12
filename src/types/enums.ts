4bec99b3a0cbc60ef7ac2451c3291c362997813b
Alexey <56958306+dante9990@users.noreply.github.com>
2024-10-22 18:46:42 +0400

    first commit

---
+++ src/types/enums.ts
@@ -0,0 +1,15 @@
+export enum NewsCategory {
+    Business='business',
+    Entertaiment='entertaiment',
+    General='general',
+    Health='health',
+    Science='science',
+    Technology='technology'
+}
+
+export enum ErrorType {
+    Auth='auth',
+    NotFound='not_found',
+    Server='server',
+    Unknown='unknown'
+}
